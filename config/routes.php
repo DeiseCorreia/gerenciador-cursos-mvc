cb9bcad413b3388b2afd67583eb822e1eace09b5
Deise Correia <deisecorreia34@gmail.com>
2021-07-07 21:08:37 -0300

    commit

--- config/routes.php
+++ config/routes.php
@@ -1,13 +1,17 @@
 <?php
 
-use Alura\Cursos\Controller\ListarCursos;
-use Alura\Cursos\Controller\Persistencia;
-use Alura\Cursos\Controller\FormularioInsercao;
+use Alura\Cursos\Controller\{
+    Exclusao,
+    ListarCursos,
+    Persistencia,
+    FormularioInsercao
+};
 
 //devolve as rotas para o index.php
 
 return [
     '/listar-cursos' => ListarCursos::class,
     '/novo-curso'=>FormularioInsercao::class,
-    '/salvar-curso'=> Persistencia::class
+    '/salvar-curso'=> Persistencia::class,
+    '/excluir-curso'=> Exclusao::class
 ];
